98258dc0655da9e7b383192309b876156f848054
farahelmashad <farahalmashad75gmail.com>
2024-12-04 00:59:23 +0200

    First commit

---
+++ src/mainPackage/User.java
@@ -0,0 +1,32 @@
+package mainPackage;
+
+public class User {
+    private String Email;
+    private String Password;
+
+    public User() {
+    }
+
+    public User(String email, String password) {
+        Email = email;
+        Password = password;
+
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+}
